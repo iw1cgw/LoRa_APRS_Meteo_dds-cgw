740c250c0b69d88f7cc22bc6dfaa14f470cbbdf2
iw1cgw <139268257+iw1cgw@users.noreply.github.com>
2023-07-11 21:01:44 +0200

    Add files via upload
    in config entry of altitude value for SLM pressure calculation from Bosch datasheets formulas
    in website.h cosmetic changes following insertion of hygrometer and removal of anemometer

--- include/config.h
+++ include/config.h
@@ -1,23 +1,24 @@
 // GENERAL SETTINGS
 
 #define Use_WiFi true                   // enable Wi-Fi connection and HTTP web server
-const char* ssid = "SSID";              // WiFi SSID
-const char* password = "mywifipassword";		// WiFi Password
+const char* ssid = "iq1bp-wl";              // WiFi SSID
+const char* password = "dciarisez";		// WiFi Password
 #define Hostname "LORAMETEO"         // Hostname, name of computer visible in the network
 
 // METEO SERVICE SETTINGS
 
 #define USE_METEO true                 // beacon meteo packets and store values for graphs
-#define METEO_CALLSIGN "N0CALL-6"
-#define METEO_BEACON 15
-#define METEO_LAT "4000.00N"
-#define METEO_LON "01600.00E"
+#define METEO_CALLSIGN "IW1CGW-3"
+#define METEO_BEACON 10
+#define METEO_LAT "4423.28N"
+#define METEO_LON "00748.87E"
+#define METEO_ALTITUDE "650"
 #define METEO_COMMENT "LoRa IGate&Meteo"
-#define USE_METEO_STATUS false           // send status below in timeout of igate packet (needs wifi, igate on and aprs-is)
-#define METEO_STATUS "e.g. your website link"
+#define USE_METEO_STATUS true           // send status below in timeout of igate packet (needs wifi, igate on and aprs-is)
+#define METEO_STATUS "LoRa Meteo based by OK2DDS project"
 #define GRAPH_LIMIT 144                   // how many values to store for graphs (too high can cause errors)
 
-#define USE_ANEMOMETER true                     // turn on/off wind meter
+#define USE_ANEMOMETER false                     // turn on/off wind meter
 #define HALL_SENSOR_PIN 35                 // use only ADC_1 pins
 #define ANEMOMETER_LENGTH 0.25           // how long distance (meters) is done by spinning anemometer from one magnet to the next one
 #define ANEMO_RECALC_LIMIT 2            // calibrate hall sensor
@@ -33,17 +34,17 @@ const char* password = "mywifipassword";		// WiFi Password
 
 #define Use_IGATE true              // send received packets to APRS-IS, needs Wi-Fi turned on
                                     // Igate can be turned off with /switch-aprs, but can not be turned on if it's disabled by this
-#define IGATE_BEACON 59
+#define IGATE_BEACON 10
 #define IGATE_BCN_NETWORK true              // if true, will send igate beacon via aprs-is, if false will send via LoRa
-#define IGATE_LAT "4000.00N"
-#define IGATE_LON "01600.00E"
-#define IGATE_COMMENT "LoRa IGate&Meteo"
-#define IGATE_CALLSIGN "N0CALL-1"
+#define IGATE_LAT "4423.74N"
+#define IGATE_LON "00748.54E"
+#define IGATE_COMMENT "LoRa IGate based by OK2DDS project"
+#define IGATE_CALLSIGN "IW1CGW-10"
 #define APRS_IS_Server "rotate.aprs2.net"    // APRS-IS server for igate
 #define APRS_IS_Port 14580                  // server port, keep default (14580)
-#define APRS_IS_Password 10000              // Your APRS-IS password
+#define APRS_IS_Password 19617              // Your APRS-IS password
 
-#define USE_DIGIPEATER true                 // allow digipeating of packets
+#define USE_DIGIPEATER false                 // allow digipeating of packets
 #define USE_LASTRX_STATUS true              // display Last RX status on igate
 
 // HTTP GET DATA UPLOAD SERVICE SETTINGS (experimental, for development)
@@ -56,8 +57,8 @@ const char* password = "mywifipassword";		// WiFi Password
 
 #define SERIAL_BAUD 9600                           // serial baud
 
-#define LoRa_Frequency 433775E3                    // Frequency (433775000 Hz)
-#define LoRa_TxPower 20                            // TX power (max 20 dBm)
+#define LoRa_Frequency 868200E3                    // Frequency (433775000 Hz)
+#define LoRa_TxPower 10                            // TX power (max 20 dBm)
 
 #define LoRa_SCK 5
 #define LoRa_MISO 19
@@ -65,6 +66,7 @@ const char* password = "mywifipassword";		// WiFi Password
 #define LoRa_SS 18
 #define LoRa_RST 14
 #define LoRa_DIO0 26
+
 #define LoRa_SpreadingFactor 12
 #define LoRa_SignalBandwidth 125000
 #define LoRa_CodingRate4 5
