3e1e7a29d830e0f8b302b8bceba1f9a7df246ffb
iw1cgw <139268257+iw1cgw@users.noreply.github.com>
2024-02-07 23:16:16 +0100

    Delete tools directory

--- tools/mergebins.bat
+++
@@ -1,2 +0,0 @@
-@echo on
-esptool --chip ESP32 merge_bin -o LoRa_APRS_Meteo+iGate_DDS_CGW_0.00_20230101.bin --flash_mode dio --flash_size 4MB 0xe000 boot_app0.bin 0x1000 bootloader.bin 0x8000 partitions.bin 0x10000 firmware.bin 
