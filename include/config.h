fe9b7423b70132e80b5736a599d0b9337e74a3e3
iw1cgw <139268257+iw1cgw@users.noreply.github.com>
2024-03-05 13:48:15 +0100

    Add files via upload

--- include/config.h
+++ include/config.h
@@ -2,17 +2,57 @@
 
 #define Project "LoRa_APRS_iw1cgw"
 #define Release "1.02"
-char Build[7] = "240227";
+char Build[7] = "240302";
 #define Hostname "LoRa_APRS_iw1cgw"         // Hostname, name of computer visible in the network
 
+/*
+ - tocall: APLHI?
+   vendor: Giovanni, IW1CGW
+   model: LoRa IGate/Digipeater/Telemetry
+   class: digi
+   contact: iw1cgw@libero.it
+
+ - tocall: APLHM?
+   vendor: Giovanni, IW1CGW
+   model: LoRa Meteostation
+   class: wx
+   contact: iw1cgw@libero.it
+
+RAM:   [=         ]  14.8% (used 48556 bytes from 327680 bytes)
+Flash: [========  ]  84.7% (used 1109585 bytes from 1310720 bytes)
+
+RAM:   [=         ]  14.8% (used 48588 bytes from 327680 bytes)
+Flash: [========= ]  85.4% (used 1119861 bytes from 1310720 bytes)
+
+RAM:   [=         ]  14.8% (used 48572 bytes from 327680 bytes)
+Flash: [========= ]  85.3% (used 1117665 bytes from 1310720 bytes)
+
+*/
 
 
+char OTA_server[15] ="ftpupload.net";       // verificare che l'array sia n+1 carattere del valore
+char OTA_user[15] = "if0_35994920";         // verificare che l'array sia n+1 carattere del valore
+char OTA_pass[15] = "HaAQ8pdOzLMtWb";       // verificare che l'array sia n+1 carattere del valore
+
+#define OTA_dirName "/htdocs/"
+#define OTA_fileName_logbook "data.txt"        // file testo che contiene la progressione dei codici OTA
+#define OTA_fileName_ver "DSCN0001.JPG"       // file testo che contiene unicamente il valore della Build del file di upload
+#define OTA_fileName_update "DSCN0002.JPG"     // il file binario di upload
+#define OTA_web_server "http://iv3sgb.000.pe/"
 
 
 
 
 
 #define GRAPH_LIMIT 144                   // how many values to store for graphs (too high can cause errors)
+#define USE_ANEMOMETER false              // turn on/off wind meter
+#define HALL_SENSOR_PIN 35                // use only ADC_1 pins
+#define ANEMOMETER_LENGTH 0.25            // how long distance (meters) is done by spinning anemometer from one magnet to the next one
+#define ANEMO_RECALC_LIMIT 2              // calibrate hall sensor
+#define ANEMO_AC_THRESHOLD 1830           // analog value threshold to trigger magnet detection
+#define ANEMO_AC_LOSE      2000           // analog value threshold - magnet is away
+#define ANEMO_RECALC_LIMIT_TIMEOUT 900    // auto update long-period speed after x seconds (should be METEO_BEACON * 60)
+#define ANEMO_RECALC_ACTUAL_SPEED 4       // set actual wind speed to 0 if anemometer is not spinning for x seconds
 
 #define APRS_IS_Port 14580                  // server port, keep default (14580)
 
@@ -42,6 +82,7 @@ char Build[7] = "240227";
 
 // DEBUG SETTINGS
 
+#define ANEMO_DEBUG_MODE false                        // enable debug mode for hall sensor
 #define HTTP_DEBUG_MODE false                          // print incoming HTTP requests
 // EXPERIMENTAL DEBUG ONLY SETTINGS
 #define DIGI_IGNORE_PARADIGM false       // digipeat packets regardless if they contain WIDEn-N
--- include/website.h
+++ include/website.h
@@ -1,7 +1,7 @@
 #define webPageStart "<!DOCTYPE html><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;} body { height: 99%;} footer { color: #696969; width: 100%; text-align: center;} a { color: #696969; font-family: Helvetica;} a:visited { color: #696969;} a:hover { color: #002fba;} table { width: 100%; text-align: center; margin-left: auto; margin-right: auto; table-layout: fixed;} .tr { line-height: 300%;} .header { font-size: 12pt;} .watchheader { font-size: 26pt;} .value { font-weight: bold; font-size: 16pt;} .watchvalue { font-weight: bold; font-size: 28pt;} b { padding: 7px; border-radius: 5px;} .good { color: white; background: #00702f;} .bad { color: white; background: red;} .cold { color: #004594;} .hot { color: #940000;} .charged { color: #00702f;}</style><title>LoRa Meteo</title></head><body>"
 #define webPageHeader "<h1>LoRa Meteostation"
 //#define webPageFooter "<br><br><footer>Created by <a href=\"https://www.ok2dds.cz\">OK2DDS</a> - Modified by <a href=\"https://iw1cgw.wordpress.com\">IW1CGW</a><br><br>   <a href=\"/graphs\">Weather charts</a><br><a href=\"/lora\">View dashboard</a><br><br>Created by <a href=\"https://www.ok2dds.cz\">OK2DDS</a> - Modified by <a href=\"https://iw1cgw.wordpress.com\">IW1CGW</a></footer>"
-#define webPageFooter "<br><br><br><footer><a href='/'>view main meteo page</a> <br> <a href=\"/graphs\">weather charts</a> <br> <a href=\"/lora\">view dashboard</a><br><br>by <a href=\"https://iw1cgw.wordpress.com\">IW1CGW</a> based on <a href=\"https://www.ok2dds.cz\">OK2DDS</a> 'project</footer>"
+#define webPageFooter "<br><br><br><footer><a href='/'>view main meteo page</a> <br> <a href=\"/graphs\">weather charts</a> <br> <a href=\"/lora\">view dashboard</a> - <a href=\"https://iw1cgw.wordpress.com/lora-aprs-meteoigate\">user manual</a><br><br>by <a href=\"https://iw1cgw.wordpress.com\">IW1CGW</a> based on <a href=\"https://www.ok2dds.cz\">OK2DDS</a> 'project</footer>"
 #define webPageEnd "</body></html>"
 #define webReload "<script>window.location.href = '/lora';</script>"
 //#define webTest "<script>window.location.href = '/test';</script>"
@@ -13,9 +13,10 @@
 #define web_ChangePrompt_temp "<script>window.location.href = '/new-value/' + prompt('new value thermal drift (max +/- 5) | ex: -0.25', null);</script>"
 #define web_ChangePrompt_pres "<script>window.location.href = '/new-value/' + prompt('new value pressure drift (max +/- 10) | ex: -2', null);</script>"
 
-#define web_ChangePrompt_power "<script>window.location.href = '/new-value/' + prompt('new value power (max 20 dbm) | ex: 2', null);</script>"
+#define web_ChangePrompt_power  "<script>window.location.href = '/new-value/' + prompt('new value power (max 20 dbm) | ex: 2', null);</script>"
 #define web_ChangePrompt_radius "<script>window.location.href = '/new-value/' + prompt('new value Km digipeater radius (max 30) | ex: 12', null);</script>"
-#define web_ChangePrompt_blacklist "<script>window.location.href = '/new-value/' + prompt('station temporarily blacklisted | ex: IZ1XYZ', null);</script>"
+#define web_ChangePrompt_route "<script>window.location.href = '/new-value/' + prompt('digipeat route is ONLY for: | ex: IR1XYZ', null);</script>"
+#define web_ChangePrompt_banned "<script>window.location.href = '/new-value/' + prompt('digipeat route denied for: | ex: IR1XYZ', null);</script>"
 
 //#define webAPRSISChangePrompt "<script>window.location.href = '/new-aprsis/' + prompt('Enter new APRS-IS server address', null);</script>"
 //#define webAPRSISChangeError "<script>alert('Error setting new APRS-IS server'); window.location.href = '/lora';</script>"
